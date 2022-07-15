5bc86c84518440e7d7e9dcef29ff8a99af8fc5fc
Anosh Khan <anoshkhanakb123@gmail.com>
2022-07-15 21:57:23 +0500

    nav

--- src/component/Basics/Restaurant.jsx
+++ src/component/Basics/Restaurant.jsx
@@ -4,8 +4,25 @@ import Menu from './menuApi.js';
 import MenuCard from "./MenuCard";
 const Restaurant = () =>{
     const[menuData , setMenuData] = useState(Menu);
+
+    const filterItem = (category) => {
+        const updatedList = Menu.filter((curElem) => {
+            return curElem.category == category;
+        });
+        setMenuData(updatedList);
+    }
+
     return(
         <>
+            <nav className="navbar">
+                <div className="btn-group">
+                    <button className="btn-group__item" onClick={() => filterItem("breakfast")}>Breakfast</button>
+                    <button className="btn-group__item" onClick={() => filterItem("lunch")}>Lunch</button>
+                    <button className="btn-group__item" onClick={() => filterItem("evening")}>Evening</button>
+                    <button className="btn-group__item" onClick={() => filterItem("dinner")}>Dinner</button>
+                    <button className="btn-group__item" onClick={() => setMenuData(Menu)}>All</button>
+                </div>
+            </nav>        
            <MenuCard menuData = {menuData} /> 
         </>
     );
